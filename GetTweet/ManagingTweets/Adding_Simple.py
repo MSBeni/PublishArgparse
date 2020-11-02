b80877b48784eb37e734316583648d5482daf0d5
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-11-02 14:49:08 -0500

    Added GetTweet file 4

--- GetTweet/ManagingTweets/Adding_Simple.py
+++ GetTweet/ManagingTweets/Adding_Simple.py
@@ -2,6 +2,7 @@ class AddingSimple:
     def __init__(self):
         pass
 
-    def addOne(self, x):
-        return x+1
+    def print_till_100(self):
+        for i in range(1, 101):
+            return i
 
---
+++ GetTweet/__main__.py
@@ -0,0 +1,45 @@
+import argparse
+import sys
+
+from ManagingTweets.Adding_Simple import AddingSimple
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--counttill100', required=False, action='store_true', help='Print count till 100')
+
+    return parser
+
+
+def main(argv=None):
+    """
+    :desc: Entry point method
+    """
+    if argv is None:
+        argv = sys.argv
+
+    try:
+        parser = create_parser()
+        args = parser.parse_args(argv[1:])
+
+        # Arguments initialization
+        counttill100 = args.counttill100
+
+        # Parser check
+        if counttill100:
+            print(AddingSimple.print_till100())
+
+    except KeyboardInterrupt:
+        print('\nGood Bye.')
+
+    return 0
+
+
+def counttill100fun():
+    for i in range(1, 101):
+        print(i)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
