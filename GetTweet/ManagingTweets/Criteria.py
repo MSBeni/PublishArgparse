528f4d4699a5698b04fcdf129ab1d5e99442d37f
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-11-03 16:32:46 -0500

    Added the criteria class

---
+++ GetTweet/ManagingTweets/Criteria.py
@@ -0,0 +1,35 @@
+class ArgsCriteria:
+    """Search parameters class"""
+
+    def __init__(self):
+        self.maxTweets = 0
+        self.topTweets = False
+        self.within = "15mi"
+        self.emoji = "ignore"
+
+    def setcounttill100(self, ct100):
+        """Set the permission to count to 100
+        Parameters
+        ----------
+        ct100 : True or false
+        """
+        self.ct100 = ct100
+        return self
+
+    def setExcludeWords(self, print1):
+        """Set the permission to print name
+        Parameters
+        ----------
+        print1 : True or false
+        """
+        self.print1 = print1
+        return self
+
+    def setSince(self, name):
+        """Set the name of the guy you want to say hello
+        Parameters
+        ----------
+        name : str,
+        """
+        self.name = name
+        return self
